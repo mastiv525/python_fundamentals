04d8a170992cb4470e2155a0c5af6a830af7478d
mastiv525 <alanski2002@gmail.com>
2024-03-20 20:12:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import math
+
+students_count = 100
+rating = 4.99
+is_okay = True
+
+random_name = "Barad dur"
+new_name = "this city will be a \"Minas Durum\" now"
+title = "Lord of the Rings"
+
+print(students_count)
+print(len(random_name))
+
+
+print(random_name[0])
+print(random_name[0:3])
+print(random_name[3:9])
+print(random_name[-2])
+print(new_name)
+print(f"{random_name} {new_name}")
+print(title.capitalize())
+print(title.lower())
+print(title.upper())
+print(title.find("the"))
+print(title.replace("R", "B"))
+print("of" in title)
+print("ok" not in title)
+
+
+print(round(2.9))
+print(abs(-2.9))
+print(math.ceil(23.5))
+
+
+temperature = 15
+
+if temperature > 30:
+    print("it's warm")
+    print("drink water")
+elif temperature > 20:
+    print("it's nice")
+else:
+    print("it's cold")
+
+#ternary operator
+
+age = 22
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not eligible"
+print(message)
+
+#logical operators
+
+high_income = False
+good_credit = True
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+if high_income or good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+#chaining comparison
+
