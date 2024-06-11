03cc4facadc7e87f805bcbecd38bca2b04e5ebe7
mastiv525 <alanski2002@gmail.com>
2024-06-11 22:26:47 +0200

    more about magic methods

--- main.py
+++ main.py
@@ -539,9 +539,11 @@ class Point:
     def __gt__(self, other):
         return self.x > other.x and self.y > other.y
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
     def draw(self):
         print(f"Point ({self.x, self.y})")
-
 
 
 point = Point(1, 2)
@@ -550,6 +552,43 @@ point3 = Point(5, 6)
 point4 = Point(8, 10)
 
 point.draw()
+point2.draw()
+point3.draw()
+point4.draw()
+
 print(point)
 print(point == point2)
 print(point4 > point3)
+print(point2 + point3)
+
+
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+cloud = TagCloud()
+
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+
+print(cloud.tags)
+print(len(cloud))
+cloud["python"] = 10
+for tag in cloud:
+    print(tag)
