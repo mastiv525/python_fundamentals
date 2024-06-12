b0a3cd6446b91d3562a72fe860eff27accce87fc
mastiv525 <alanski2002@gmail.com>
2024-06-12 20:56:46 +0200

    intro to Inheritance

--- main.py
+++ main.py
@@ -611,3 +611,24 @@ class Product:
 
 product = Product(50)
 print(product.price)
+
+######## INHERITANCE
+
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+m = Mammal()
+m.eat()
+print(m.age)
