c360bfe13c33aa8e34d0e71dbbe64c9c4f124571
mastiv525 <alanski2002@gmail.com>
2024-06-15 22:27:49 +0200

    good example of inheritance

--- main.py
+++ main.py
@@ -641,3 +641,28 @@ m.eat()
 print(m.age)
 print(m.weight)
 
+class InvalidOperationEror(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationEror("Stream is already opened.")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationEror("Stream is already closed.")
+        self.opened = False
+
+class FileStream(Stream):
+    def read(self):
+        print("Readind data from a file")
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network")
