dd7db13b7bb3212d7323a8b41857a6c6808720bf
mastiv525 <alanski2002@gmail.com>
2024-06-25 20:24:05 +0200

    operations with zip archives

--- work_with_other/zip.py
+++ work_with_other/zip.py
@@ -5,5 +5,9 @@ with ZipFile("files.zip", "w") as zip:
     for path in Path("work_with_other").rglob("*.*"):
         zip.write(path)
 
-# with ZipFile("files.zip") as zip:
-#     print(zip.namelist()
+with ZipFile("files.zip") as zip:
+    print(zip.namelist())
+    info = zip.getinfo("work_with_other/path.py")
+    print(info.file_size)
+    print(info.compress_size)
+    zip.extract("extract")
