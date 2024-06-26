8c3d93fe65549dddb108e420f665e324e1745d54
mastiv525 <alanski2002@gmail.com>
2024-06-26 19:25:09 +0200

    .

--- work_with_other/test_json.py
+++ work_with_other/test_json.py
@@ -4,7 +4,11 @@ from pathlib import Path
 
 movies = [
     {"id": 1, "title": "Phantom Menace", "year": 1999},
-    {"id": 2, "title": "Attack of Clones", "year": 2001},
+    {"id": 2, "title": "Attack of the Clones", "year": 2002},
+    {"id": 3, "title": "Revenge of the Sith", "year": 2005},
+    {"id": 4, "title": "A New Hope", "year": 1977},
+    {"id": 5, "title": "Phantom Menace", "year": 1980},
+    {"id": 6, "title": "Phantom Menace", "year": 1983},
 ]
 
 data = json.dumps(movies)
