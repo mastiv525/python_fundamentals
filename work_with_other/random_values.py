73b565e573556108c20e7a947844330627cd6418
mastiv525 <alanski2002@gmail.com>
2024-07-02 22:31:25 +0200

    random values

---
+++ work_with_other/random_values.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+print(random.random())
+print(random.randint(1, 10))
+print(random.choice([1, 2, 3, 4]))
+print(random.choices([1, 2, 3, 4], k=2))
+print(random.choices("ghadashtrjhgpokdaswejwiq", k=4))
+print("".join(random.choices(string.ascii_letters + string.digits, k=4)))
+
+numbers = [1, 2, 3, 4]
+random.shuffle(numbers)
+print(numbers)
